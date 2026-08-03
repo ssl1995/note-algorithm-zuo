bd54d2f1e662bfeb9b036426ed847f24a9342f68
songshenglin <1363912005@163.com>
2026-08-03 17:00:22 +0800

    位图和加减乘除

--- src/class031/Code03_Near2power.java
+++ src/class031/Code03_Near2power.java
@@ -5,22 +5,26 @@ package class031;
 // 如果int范围内不存在这样的数，返回整数最小值
 public class Code03_Near2power {
 
-	public static final int near2power(int n) {
-		if (n <= 0) {
-			return 1;
-		}
-		n--;
-		n |= n >>> 1;
-		n |= n >>> 2;
-		n |= n >>> 4;
-		n |= n >>> 8;
-		n |= n >>> 16;
-		return n + 1;
-	}
+  public static int near2power(int n) {
+    if (n <= 0) {
+      return 1;
+    }
+		// 1、>=n 转成 严格>n-1的最小2的幂
+		// 如果n不是2的幂，那么n--没用；但如果n是2的幂，答案就是自己
+    n--;
+		// 2、最高位的1向右铺满
+    n |= n >>> 1;
+    n |= n >>> 2;
+    n |= n >>> 4;
+    n |= n >>> 8;
+    n |= n >>> 16;
+		// 3、返回n+1，就是>=n的最小2的幂次
+    return n + 1;
+  }
 
-	public static void main(String[] args) {
-		int number = 100;
-		System.out.println(near2power(number));
-	}
+  public static void main(String[] args) {
+    int number = 100;
+    System.out.println(near2power(number));
+  }
 
 }
--- src/class032/Code02_DesignBitsetTest.java
+++ src/class032/Code02_DesignBitsetTest.java
@@ -12,98 +12,107 @@ package class032;
 // String toString() : 返回所有位的状态
 public class Code02_DesignBitsetTest {
 
-	// 测试链接 : https://leetcode-cn.com/problems/design-bitset/
-	class Bitset {
-		private int[] set;
-		private final int size;
-		private int zeros;
-		private int ones;
-		private boolean reverse;
+  // 测试链接 : https://leetcode-cn.com/problems/design-bitset/
+  static class Bitset {
+    private int[] set;
+    private final int size;
+    // 本题的特殊设计
+    private int zeros;
+    private int ones;
+    private boolean reverse;
 
-		public Bitset(int n) {
-			set = new int[(n + 31) / 32];
-			size = n;
-			zeros = n;
-			ones = 0;
-			reverse = false;
-		}
+    public Bitset(int n) {
+      // 向上取整
+      set = new int[(n + 32 - 1) / 32];
+      size = n;
+      zeros = n;
+      ones = 0;
+      reverse = false;
+    }
 
-		// 把i这个数字加入到位图
-		public void fix(int i) {
-			int index = i / 32;
-			int bit = i % 32;
-			if (!reverse) {
-				// 位图所有位的状态，维持原始含义
-				// 0 : 不存在
-				// 1 : 存在
-				if ((set[index] & (1 << bit)) == 0) {
-					zeros--;
-					ones++;
-					set[index] |= (1 << bit);
-				}
-			} else {
-				// 位图所有位的状态，翻转了
-				// 0 : 存在
-				// 1 : 不存在
-				if ((set[index] & (1 << bit)) != 0) {
-					zeros--;
-					ones++;
-					set[index] ^= (1 << bit);
-				}
-			}
-		}
+    // 把i这个数字加入到位图
+    public void fix(int i) {
+      int index = i / 32;
+      int bit = i % 32;
+      if (!reverse) {
+        // 位图所有位的状态，维持原始含义
+        // 0 : 不存在
+        // 1 : 存在
+        if ((set[index] & (1 << bit)) == 0) {
+          zeros--;
+          ones++;
+          set[index] |= (1 << bit);
+        }
+      } else {
+        // 位图所有位的状态，翻转了
+        // 0 : 存在
+        // 1 : 不存在
+        if ((set[index] & (1 << bit)) != 0) {
+          zeros--;
+          ones++;
+          set[index] ^= (1 << bit);
+        }
+      }
+    }
 
-		// 把i这个数字从位图中移除
-		public void unfix(int i) {
-			int index = i / 32;
-			int bit = i % 32;
-			if (!reverse) {
-				if ((set[index] & (1 << bit)) != 0) {
-					ones--;
-					zeros++;
-					set[index] ^= (1 << bit);
-				}
-			} else {
-				if ((set[index] & (1 << bit)) == 0) {
-					ones--;
-					zeros++;
-					set[index] |= (1 << bit);
-				}
-			}
-		}
+    // 把i这个数字从位图中移除
+    public void unfix(int i) {
+      int index = i / 32;
+      int bit = i % 32;
+      if (!reverse) {
+        if ((set[index] & (1 << bit)) != 0) {
+          ones--;
+          zeros++;
+          set[index] ^= (1 << bit);
+        }
+      } else {
+        if ((set[index] & (1 << bit)) == 0) {
+          ones--;
+          zeros++;
+          set[index] |= (1 << bit);
+        }
+      }
+    }
 
-		public void flip() {
-			reverse = !reverse;
-			int tmp = zeros;
-			zeros = ones;
-			ones = tmp;
-		}
+    public void flip() {
+      reverse = !reverse;
+      int tmp = zeros;
+      zeros = ones;
+      ones = tmp;
+    }
 
-		public boolean all() {
-			return ones == size;
-		}
+    public boolean all() {
+      return ones == size;
+    }
 
-		public boolean one() {
-			return ones > 0;
-		}
+    public boolean one() {
+      return ones > 0;
+    }
 
-		public int count() {
-			return ones;
-		}
+    public int count() {
+      return ones;
+    }
 
-		public String toString() {
-			StringBuilder builder = new StringBuilder();
-			for (int i = 0, k = 0, number, status; i < size; k++) {
-				number = set[k];
-				for (int j = 0; j < 32 && i < size; j++, i++) {
-					status = (number >> j) & 1;
-					status ^= reverse ? 1 : 0;
-					builder.append(status);
-				}
-			}
-			return builder.toString();
-		}
+    public String toString() {
+      StringBuilder builder = new StringBuilder();
+      for (int i = 0, k = 0, number, status; i < size; k++) {
+        number = set[k];
+        for (int j = 0; j < 32 && i < size; j++, i++) {
+          status = (number >> j) & 1;
+          status ^= reverse ? 1 : 0;
+          builder.append(status);
+        }
+      }
+      return builder.toString();
+    }
 
-	}
+    public static void main(String[] args) {
+      Bitset bitset = new Bitset(2);
+      bitset.fix(1);
+      bitset.fix(2);
+      System.out.println(bitset);
+    }
+
+  }
 
 }
